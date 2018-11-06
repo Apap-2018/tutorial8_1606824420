26e9522ead000c15df5b6f58edcb127b2d40a3c0
wildaparasti <wildasti98@gmail.com>
2018-11-06 22:56:00 +0700

    tutorial 8 done

---
+++ src/main/java/com/apap/tutorial8/controller/UserRoleController.java
@@ -0,0 +1,93 @@
+package com.apap.tutorial8.controller;
+
+import java.util.regex.Pattern;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.apap.tutorial8.model.PasswordModel;
+import com.apap.tutorial8.model.UserRoleModel;
+import com.apap.tutorial8.service.UserRoleService;
+
+@Controller
+@RequestMapping("/user")
+public class UserRoleController {
+	@Autowired
+	private UserRoleService userService;
+	
+	@RequestMapping(value="/addUser", method=RequestMethod.POST)
+	private ModelAndView addUser(@ModelAttribute UserRoleModel user, RedirectAttributes redir) {
+	    String pattern = "(?=.*[0-9])(?=.*[a-zA-Z]).{8,}";
+	    String message = "";
+	    System.out.println(user.getPassword().matches(pattern) + " MASUK POST METHOD : "+user.getPassword());
+		if(user.getPassword().matches(pattern)){
+			userService.addUser(user);
+			message = null;
+			
+		}
+		else {
+			message = "password anda harus mengandung angka, huruf kecil, dan memiliki setidaknya 8 karakter";
+		}
+		
+		ModelAndView modelAndView = new ModelAndView("redirect:/");
+		redir.addFlashAttribute("msg",message);
+		return modelAndView;
+	}
+	@RequestMapping("/updatePassword")
+	public String updatePassword() {
+		return "passwordUpdate";
+	}
+	
+	public boolean validatePassword(String password) {
+		if (password.length()>=8 && Pattern.compile("[0-9]").matcher(password).find() &&  Pattern.compile("[a-zA-Z]").matcher(password).find())  {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	@RequestMapping(value="/passwordSubmit",method=RequestMethod.POST)
+	public ModelAndView updatePasswordSubmit(@ModelAttribute PasswordModel pass, Model model,RedirectAttributes redir) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		UserRoleModel user = userService.findUserByUsername(SecurityContextHolder.getContext().getAuthentication().getName());
+		String message = "";
+		
+		if (pass.getConPassword().equals(pass.getNewPassword())) {
+			
+			if (passwordEncoder.matches(pass.getOldPassword(), user.getPassword())) {
+				if (validatePassword(pass.getNewPassword())) {
+					userService.changePassword(user, pass.getNewPassword());
+					message = "password berhasil diubah";
+				}
+				else {
+					message = "password baru anda belum sesuai ketentuan: lebih dari 8 karakter, mengandung minimal 1 huruf dan 1 angka";
+				}
+				
+			}
+			else {
+				message = "password lama anda salah";
+			}
+			
+		}
+		else {
+			message = "password baru tidak sesuai";
+		}
+		
+		
+		ModelAndView modelAndView = new ModelAndView("redirect:/user/updatePassword");
+		redir.addFlashAttribute("msg",message);
+		return modelAndView;
+	}
+}
